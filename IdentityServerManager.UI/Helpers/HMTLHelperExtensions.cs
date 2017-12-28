c0a3e5ebb2c814ba29fb78b1bf2401d859099337
Alan Macgowan <alanmacgowan@gmail.com>
2017-12-28 00:17:18 -0300

    applied Paper Dashboard theme

---
+++ IdentityServerManager.UI/Helpers/HMTLHelperExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+
+namespace IdentityServerManager.UI.Helpers
+{
+    public static class HMTLHelperExtensions
+    {
+        public static string IsSelected(this IHtmlHelper html, string controller = null, string action = null, string cssClass = null)
+        {
+
+            if (String.IsNullOrEmpty(cssClass))
+                cssClass = "active";
+
+            string currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            string currentController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            if (String.IsNullOrEmpty(controller))
+                controller = currentController;
+
+            if (String.IsNullOrEmpty(action))
+                action = currentAction;
+
+            return controller == currentController && action == currentAction ?
+                cssClass : String.Empty;
+        }
+
+        public static string PageClass(this HtmlHelper html)
+        {
+            string currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            return currentAction;
+        }
+    }
+
+}
