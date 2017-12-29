33a4fb9096cec5d85dce39b3633ec050511f1de4
Alan Macgowan <alanmacgowan@gmail.com>
2017-12-29 17:30:13 -0300

    added dataanotations to api resources vm

--- IdentityServerManager.UI/Models/ApiResourceViewModel.cs
+++ IdentityServerManager.UI/Models/ApiResourceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer4.EntityFramework.Entities;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IdentityServerManager.UI.Models
 {
@@ -7,8 +8,15 @@ namespace IdentityServerManager.UI.Models
     {
         public int Id { get; set; }
         public bool Enabled { get; set; } = true;
+        [Required]
+        [StringLength(200)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Display Name")]
         public string DisplayName { get; set; }
+        [StringLength(1000)]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public List<ApiSecret> Secrets { get; set; }
         public List<ApiScope> Scopes { get; set; }
